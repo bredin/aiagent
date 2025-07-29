a40fc5b483a391cd4f30e3fa4f56c3226086c36b
bredin <insanityindustries@gmail.com>
2025-07-30 06:31:34 +1200

    verbose output

--- main.py
+++ main.py
@@ -9,10 +9,9 @@ def main():
     if len(sys.argv) < 2:
         print("Error: No prompt provided. Please provide a prompt as a command line argument.")
         sys.exit(1)
-
+    verbose = (len(sys.argv) > 2) and (sys.argv[2] == "--verbose")
+    
     prompt = sys.argv[1]
-
-    print("Hello from aiagent!")
 
     load_dotenv()  # Load environment variables from .env file
     gemini_api_key = os.getenv("GEMINI_API_KEY")
@@ -22,12 +21,13 @@ def main():
 
     response = client.models.generate_content(model=model, contents=prompt)
 
-    print("Response from Gemini API:") 
-    print(response.text)
-    print("Done!")
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-
+    if verbose:
+        print(f"User prompt: {prompt}")
+        print(response.text)
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    else:   
+        print(response.text)
 
 
 if __name__ == "__main__":
