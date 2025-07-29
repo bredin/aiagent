05e8b7fbad56c82dc34e58fb489c9dac763ee522
bredin <insanityindustries@gmail.com>
2025-07-30 05:37:51 +1200

    command line prompt

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 
 def main():
+    if len(sys.argv) < 2:
+        print("Error: No prompt provided. Please provide a prompt as a command line argument.")
+        sys.exit(1)
+
+    prompt = sys.argv[1]
+
     print("Hello from aiagent!")
 
-
-if __name__ == "__main__":
-    main()
     load_dotenv()  # Load environment variables from .env file
     gemini_api_key = os.getenv("GEMINI_API_KEY")
     client = genai.Client(api_key=gemini_api_key)
 
     model = "gemini-2.0-flash-001"
-    prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
 
     response = client.models.generate_content(model=model, contents=prompt)
 
@@ -23,3 +27,8 @@ if __name__ == "__main__":
     print("Done!")
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+
+
+if __name__ == "__main__":
+    main()
